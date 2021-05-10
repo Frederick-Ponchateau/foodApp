df95566622ff5cd8d4d365ed5ab295516b1daeb1
Frederick-dev23 <f.p.n.97230@gmail.com>
2021-05-10 11:37:43 -0400

    test

---
+++ component/Menu/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+const Menu = () => {
+    return (
+        <View>
+            <Text>Menu</Text>
+        </View>
+    )
+}
+
+export default Menu
